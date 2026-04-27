411455a00516b97af94bed16f36758d7a680c7ba
numb <tomizhang@outlook.com>
2026-04-27 15:10:39 +0800

    删除多余ts编译js文件

--- binance-trading-client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import './style.css'; // 稍后我们清空默认样式
-const app = createApp(App);
-app.use(createPinia());
-app.mount('#app');
